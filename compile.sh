48a271871e7b67ee31f0cb4f0e5659c3c5b760b4
Alon Sabi <alon@sabi.me>
2021-04-25 07:02:23 -0700

    Changed back to pipenv

--- compile.sh
+++ compile.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-if [ ! -d ".venv" ] 
-then
-  echo "Initializing virtual environment"
-  pyenv global 3.7.9
-  python -m venv .venv
-else
-  echo "aws-creds venv already exists"
-fi
-
 echo "Selecting aws-creds environment"
-source .venv/bin/activate
-pip install -r requirements.txt
+pipenv shell
 
 echo "Generating executable"
 pyinstaller --onefile --clean aws-creds.py 
