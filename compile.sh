c85f28af0f514e344caa8d1c2b7709b6ac221f65
Alon Sabi <alon@sabi.me>
2024-08-25 13:52:45 -0700

    Modernized to python 3.11 and removed the pipenv dependency.

--- compile.sh
+++ compile.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "Selecting aws-creds environment"
-pipenv shell
-
 echo "Generating executable"
 pyinstaller --onefile --clean aws-creds.py 
 sudo mv dist/aws-creds /usr/local/bin
